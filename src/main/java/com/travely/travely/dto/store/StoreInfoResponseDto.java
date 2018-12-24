6f6dceaa1e32ae99e089636982048f0cafb4b967
pci2676 <pci2676@gmail.com>
2018-12-24 16:35:41 +0900

    상가 목록 dto 상가번호 추가

--- src/main/java/com/travely/travely/dto/store/StoreInfoResponseDto.java
+++ src/main/java/com/travely/travely/dto/store/StoreInfoResponseDto.java
@@ -8,11 +8,13 @@ public class StoreInfoResponseDto {
 
     private long cnt;
     private String storeName;
+    private long storeIdx;
     private String localName;
 
     public StoreInfoResponseDto(final StoreJoinLocal storeJoinLocal) {
         this.storeName=storeJoinLocal.getStoreName();
         this.cnt = storeJoinLocal.getCnt();
+        this.storeIdx=storeJoinLocal.getStoreIdx();
         this.localName = storeJoinLocal.getLocalName();
     }
 }
