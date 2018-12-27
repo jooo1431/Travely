d2e8228e46c2b310048bf24968ae4bdca71556fd
tomas4540 <rudtjs4540@gmail.com>
2018-12-27 13:07:30 +0900

    favorite service 구현

--- src/main/java/com/travely/travely/domain/Favorite.java
+++ src/main/java/com/travely/travely/domain/Favorite.java
@@ -7,15 +7,15 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class favorite {
+public class Favorite {
     private long favoriteIdx;
-    private long useIdx;
+    private long userIdx;
     private long storeIdx;
 
     @Builder
-    public favorite(long favoriteIdx, long useIdx, long storeIdx) {
+    public Favorite(long favoriteIdx, long userIdx, long storeIdx) {
         this.favoriteIdx = favoriteIdx;
-        this.useIdx = useIdx;
+        this.userIdx = userIdx;
         this.storeIdx = storeIdx;
     }
 }
