e63cb8803b13d00db0230dede95af2dabd1f7462
pci2676 <pci2676@gmail.com>
2018-12-29 02:09:55 +0900

    예약 코드로 예약 상세정보 읽기
    1. reserveCode읽으면 예약 상세 내역 을 반환하는 api 작성완료
    
    2. 업주가 사진찍어서 업로드 하는부분 작업 시작

--- src/main/java/com/travely/travely/service/OwnerService.java
+++ src/main/java/com/travely/travely/service/OwnerService.java
@@ -1,8 +1,12 @@
 package com.travely.travely.service;
 
+import com.travely.travely.dto.baggage.BagDto;
+import com.travely.travely.dto.reservation.ReserveInfoDto;
 import com.travely.travely.dto.reservation.ReserveJoinPayment;
+import com.travely.travely.dto.reservation.ReservePaymentUsersDto;
 import com.travely.travely.dto.reservation.ReserveJoinStore;
 import com.travely.travely.mapper.OwnerMapper;
+import com.travely.travely.mapper.ReservationMapper;
 import com.travely.travely.util.typeHandler.ProgressType;
 import com.travely.travely.util.typeHandler.StateType;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -18,6 +23,32 @@ import java.sql.Timestamp;
 public class OwnerService {
 
     private final OwnerMapper ownerMapper;
+    private final ReservationMapper reservationMapper;
+
+    /**
+     * QR 코드 읽고 예약정보 보여주기
+     * reserveCode(예약 코드)로 현재 서비스 이용중인 사람의
+     * 예약시작일 예약번호
+     * 상세정보
+     * 예약자
+     * 예약자 연락처
+     * 결제수단
+     * 짐 타입
+     * 가격 불러오기
+     **/
+    public ReserveInfoDto readQRCode(final String reserveCode) {
+
+        //예약코드로 사용자 정보 불러옴
+        ReservePaymentUsersDto reservePaymentUsersDto = ownerMapper.getReserveInfoFindByRerserveCode(reserveCode);
+        final long reserveIdx = reservePaymentUsersDto.getReserveIdx();
+
+        //짐정보 불러옴
+        List<BagDto> bagDtos = reservationMapper.getBagDto(reserveIdx);
+        List<String> bagImgs = reservationMapper.getBaggagesImgs(reserveIdx);
+
+        return new ReserveInfoDto(reservePaymentUsersDto,bagDtos,bagImgs);
+    }
+
 
     //업주가 QR코드 리드시 상태변경해주기
     //QR을 리드 하는 상황은
@@ -29,7 +60,7 @@ public class OwnerService {
         String msg;
         final ReserveJoinPayment reserveJoinPayment = ownerMapper.getReserveJoinPaymentFindByReserveCode(reserveCode);
 
-        if(reserveJoinPayment!=null){
+        if (reserveJoinPayment != null) {
             //현금결제
             //reserve테이블의 예약완료 -> 보관중으로
             final long reserveIdx = reserveJoinPayment.getReserveIdx();
@@ -42,9 +73,9 @@ public class OwnerService {
             final ProgressType progressType = ProgressType.DONE;
             ownerMapper.setProgressToDoneOnPayment(payIdx, progressType);
 
-            return msg="READ";
-        }else{
-            return msg="NO DATA";
+            return msg = "READ";
+        } else {
+            return msg = "NO DATA";
         }
 
 
@@ -54,12 +85,34 @@ public class OwnerService {
     //업주면 true 아니면 false
     public boolean areYouOwner(final String reserveCode, final long ownerIdx) {
         final ReserveJoinStore reserveJoinStore = ownerMapper.getReserveJoinStoreFindByReserveCode(reserveCode);
-        if(reserveJoinStore!=null){
+        if (reserveJoinStore != null) {
             if (reserveJoinStore.getOwnerIdx() == ownerIdx) return true;
             else return false;
-        }
-        else return false;
+        } else return false;
 
     }
 
+
+    //업주가 짐 사진 찍을때
+    //
+    @Transactional
+    public void saveBaggagesPhotos() {
+        //사진을 찍어
+        //사진을 보여줘
+        //맘에들면 저장
+        //아니면 지우고 다시 찍어
+    }
+
+
+    //픽업 (State.TakeOff)할 때
+    //
+
+
+    //예약이 있는지 확인하여 1이면 true 0, n 개이면 false 반환
+    public boolean isReserveByReserveCode(final String reserveCode){
+        long cnt = ownerMapper.getReserveCountByReserveCode(reserveCode);
+        if(cnt==1) return true;
+        else if(cnt==0) return false;
+        else return false;
+    }
 }
--- src/main/java/com/travely/travely/web/OwnerController.java
+++ src/main/java/com/travely/travely/web/OwnerController.java
@@ -1,5 +1,6 @@
 package com.travely.travely.web;
 
+import com.travely.travely.dto.reservation.ReserveInfoDto;
 import com.travely.travely.service.OwnerService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
@@ -7,10 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
@@ -21,14 +20,55 @@ public class OwnerController {
 
     private final OwnerService ownerService;
 
-    @ApiOperation(value = "QR코드 리드", notes = "QR코드 리드후 상태변경")
+    @ApiOperation(value = "예약정보 상세확인", notes = "QR 코드 리드 혹은 reserveCode로 예약자의 예약내역 확인")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "확인성공"),
+            @ApiResponse(code = 400, message = "잘못 된 접근"),
+            @ApiResponse(code = 500, message = "서버에러")
+    })
+    @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
+    @PostMapping("/read/qr")
+    ResponseEntity<ReserveInfoDto> getReserveInfoByReserveCode(@RequestBody final String reserveCode){
+        //예약코드에 해당하는 예약이 있는지 검색
+        if(ownerService.isReserveByReserveCode(reserveCode)){
+            ReserveInfoDto reserveInfoDto = ownerService.readQRCode(reserveCode);
+            return ResponseEntity.ok().body(reserveInfoDto);
+        }else
+        {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+    }
+
+    @ApiOperation(value = "짐 사진 저장", notes = "짐 사진 저장")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "보관시작"),
             @ApiResponse(code = 400, message = "잘못 된 접근"),
             @ApiResponse(code = 500, message = "서버에러")
     })
     @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
-    @PostMapping("/qr/read")
+    @PostMapping("/reserve/bagImgs")
+    ResponseEntity<Void> saveBaggagesImgs(@RequestBody final long reserveIdx, @ApiIgnore Authentication authentication, @RequestPart(value = "bagImg")final MultipartFile[] bagImgs){
+        //업주만 저장할수 있게 한번거른다.
+        if(true){
+
+
+        }else{
+
+        }
+
+        return null;
+    }
+
+
+    @ApiOperation(value = "보관 시작", notes = "보관상태 및 결제 완료 상태가 된다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "보관시작"),
+            @ApiResponse(code = 400, message = "잘못 된 접근"),
+            @ApiResponse(code = 500, message = "서버에러")
+    })
+    @ApiImplicitParams({@ApiImplicitParam(name = "jwt", value = "JWT Token", required = true, dataType = "string", paramType = "header")})
+    @PostMapping("/reserve/confirm")
     public ResponseEntity<String> readQrCode(@RequestBody final String reserveCode, @ApiIgnore Authentication authentication) {
 
         final long ownerIdx = Long.parseLong((String) authentication.getPrincipal());
