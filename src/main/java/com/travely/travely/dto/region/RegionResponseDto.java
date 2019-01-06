1490550cbaa6e7989036a1a19aa25f9373620173
jooo1431 <joo9610@gmail.com>
2019-01-06 23:01:04 +0900

    트러블 슈팅중

--- src/main/java/com/travely/travely/dto/region/RegionResponseDto.java
+++ src/main/java/com/travely/travely/dto/region/RegionResponseDto.java
@@ -12,12 +12,10 @@ import java.util.stream.Collectors;
 public class RegionResponseDto {
 
     private String regionName;
-    private Long regionIdx;
     private List<SimpleStoreResponseDto> simpleStoreResponseDtos;
 
     public RegionResponseDto(Region region) {
         this.regionName = region.getRegionName();
-        this.regionIdx = region.getRegionIdx();
         this.simpleStoreResponseDtos = region.getStores().stream()
                 .map(store -> new SimpleStoreResponseDto(store)).collect(Collectors.toList());
     }
